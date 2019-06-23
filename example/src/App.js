caa9ca436872505de527b3355a8b05ff119970f1
mhmdtshref <mhmdtshref@gmail.com>
2019-06-24 02:34:58 +0300

    Update example App to create TextField inside Form #13

--- example/src/App.js
+++ example/src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
 
-import Form from 'form2'
+import { Form, TextField } from 'form2'
 import './App.css'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <Form fields={
-          [
-            {
-              type: 'text',
-              name: 'username',
-              placeholder: 'Type Username..',
-              id: 'username',
-              className: 'textField',
-              // eslint-disable-next-line no-console
-              onChange: (event) => { console.log(`VALUE IS: ${event.target.value}`) }
-            },
-            {
-              type: 'text',
-              name: 'password',
-              placeholder: 'Type Password..',
-              id: 'password',
-              className: 'textField'
-            }]
-        } />
+        <Form>
+          <TextField placeholder="Type your name..." />
+          <TextField placeholder="Type your email..." />
+        </Form>
       </div>
     )
   }
