3fc83cd121994670edc87a31cd67715ad7063ae1
mhmdtshref <mhmdtshref@gmail.com>
2019-06-25 14:41:33 +0300

    Handle on submit property function to add values to its event #13

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,61 +2,35 @@ import React, { Component } from 'react'
 
 class Form extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       values: {}
     }
   }
 
   setStateValue = (name, value) => {
-    this.setState((prevState) => ({...prevState.values, [name]: value}), () => {
-      console.log("STATE CHANGED:::", this.state);
+    this.setState((prevState) => ({ values: {...prevState.values, [name]: value }}), () => {
+      console.log("Submitted values are:", this.state);
     })
   };
 
-  setEventToState = ({ target }) => {
-    this.setStateValue([target.name], target.value)
+  onSubmit = (event) => {
+    event.target.values = this.state.values;
+    this.props.onSubmit(event);
   };
-
-  renderTextField = (field, index) => {
-    field.key = field.key ? field.key : index;
-    if (field.onChange) {
-      return <input
-        type='text'
-        name={field.name}
-        onChange={(event) => { field.onChange(event); this.setEventToState(event) }}
-        {...field}
-      />
-    } else {
-      return <input
-        type='text'
-        name={field.name}
-        onChange={this.setEventToState}
-        {...field}
-      />
-    }
-  };
-
-  renderFields = (fields) => {
-    return fields.map((field, index) => {
-      switch (field.type) {
-        case 'text':
-          return this.renderTextField(field, index)
-      }
-    })
-  };
-
 
   renderChildren = () => {
-    const { setStateValue } = this;
-    return React.Children.toArray(this.props.children).map((child, key) => React.cloneElement(child, { formstate: setStateValue, key }));
+    const {setStateValue} = this;
+    return React.Children.toArray(this.props.children).map((child, key) => React.cloneElement(child, {
+      formstate: setStateValue,
+      key
+    }));
   };
 
   render() {
-    return <form>
-      { this.renderChildren() }
+    return <form onSubmit={this.onSubmit}>
+      {this.renderChildren()}
     </form>
   }
 }
-
 export default Form
