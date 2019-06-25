3409062ffe1ca6feb447fca31c6334f91abfa9dd
mhmdtshref <mhmdtshref@gmail.com>
2019-06-25 14:35:28 +0300

    Add onSubmit function to form #13

--- example/src/App.js
+++ example/src/App.js
@@ -4,12 +4,19 @@ import { Form, TextField } from 'form2'
 import './App.css'
 
 export default class App extends Component {
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log("THIS IS VALUES: ", event.target.values);
+  };
+
   render () {
     return (
       <div>
-        <Form>
-          <TextField placeholder="Type your name..." name="x" />
-          <TextField placeholder="Type your email..." name="y" />
+        <Form onSubmit={this.onSubmit}>
+          <TextField placeholder="Type your name..." name="username" />
+          <TextField placeholder="Type your email..." name="email" />
+          <button>my button</button>
         </Form>
       </div>
     )
