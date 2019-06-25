d987c7b4961c1abf24256d36eb39ad77439c383a
mhmdtshref <mhmdtshref@gmail.com>
2019-06-25 12:37:58 +0300

    Update text field component to change the form state using function passed in props #13

--- src/TextField/index.js
+++ src/TextField/index.js
@@ -9,11 +9,21 @@ class TextField extends Component {
   }
 
   onChange = ({ target }) => {
-    this.setState({ value: target.value });
+    this.setState({ value: target.value }, () => {
+      this.props.formstate(this.props.name, this.state.value);
+      if(this.props.onChange) this.props.onChange();
+    });
+  };
+
+  generateProps = (props) => {
+    let newProps = {...props};
+    delete newProps.onChange;
+    delete newProps.formstate;
+    return newProps;
   };
 
   render() {
-    return <input value={this.state.value} onChange={this.onChange} {...this.props} />;
+    return <input value={this.state.value} onChange={this.onChange} {...this.generateProps(this.props)} />;
   }
 }
 
