7d93622ba15f2474d49bfd2c7407ae1f6574ca53
mhmdtshref <mhmdtshref@gmail.com>
2019-06-26 11:42:08 +0300

    Add date picker to example form #22

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Form, TextField, PasswordField } from 'form2'
+import { Form, TextField, PasswordField, DatePicker } from 'form2'
 import './App.css'
 
 export default class App extends Component {
@@ -16,6 +16,7 @@ export default class App extends Component {
         <Form onSubmit={this.onSubmit}>
           <TextField placeholder="Type your name..." name="username" />
           <PasswordField placeholder="Type new password..." name="email" />
+          <DatePicker name="birthday" />
           <button>my button</button>
         </Form>
       </div>
