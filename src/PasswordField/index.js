9fbb0ee6f6b4663afbd79249838588ef42e3487f
mhmdtshref <mhmdtshref@gmail.com>
2019-06-26 09:18:48 +0300

    Add password field compoennt #4

---
+++ src/PasswordField/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class PasswordField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  onChange = ({ target }) => {
+    this.setState({ value: target.value }, () => {
+      this.props.formstate(this.props.name, this.state.value);
+      if(this.props.onChange) this.props.onChange();
+    });
+  };
+
+  generateProps = (props) => {
+    let newProps = {...props};
+    delete newProps.onChange;
+    delete newProps.formstate;
+    return newProps;
+  };
+
+  render() {
+    return <input type="password" value={this.state.value} onChange={this.onChange} {...this.generateProps(this.props)} />;
+  }
+}
+
+export default PasswordField;
