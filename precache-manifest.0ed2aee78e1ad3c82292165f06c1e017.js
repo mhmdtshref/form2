c575d46cccf7ca820e0cb2b5aeeab78276fd38f2
mhmdtshref <mhmdtshref@gmail.com>
2019-06-26 08:54:30 +0300

    Updates

---
+++ precache-manifest.0ed2aee78e1ad3c82292165f06c1e017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873c1fff49dd935940f7956804d379ac",
+    "url": "/form2/index.html"
+  },
+  {
+    "revision": "69dd0176a92fa95bdee6",
+    "url": "/form2/static/css/main.d9a8887b.chunk.css"
+  },
+  {
+    "revision": "eacb625eecf06865a5ed",
+    "url": "/form2/static/js/2.304ab618.chunk.js"
+  },
+  {
+    "revision": "69dd0176a92fa95bdee6",
+    "url": "/form2/static/js/main.73260d29.chunk.js"
+  },
+  {
+    "revision": "316bdadbd71b199d9f95",
+    "url": "/form2/static/js/runtime~main.9bb89667.js"
+  }
+]);
