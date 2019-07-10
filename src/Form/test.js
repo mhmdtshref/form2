767b06055e78c5bd8a00d0eec9635684274ec95c
mhmdtshref <mhmdtshref@gmail.com>
2019-07-10 11:04:18 +0300

    Update form test tot cover code #38

--- src/Form/test.js
+++ src/Form/test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, mount } from 'enzyme';
+import jestMock from 'jest-mock';
 import { Form, TextField, Button, DatePicker, PasswordField } from "../";
 
 describe('Form Test', () => {
@@ -9,7 +10,7 @@ describe('Form Test', () => {
   });
 
   it('Form with one field in it', () => {
-    const form = shallow(<Form>
+    const form = render(<Form>
       <TextField name="username" />
     </Form>);
     expect(form).toMatchSnapshot();
@@ -18,12 +19,35 @@ describe('Form Test', () => {
   it('Form with multiple fields in it', () => {
     // eslint-disable-next-line no-console
     const onClickHandler = () => { console.log('Button has been clicked!') };
-    const form = shallow(<Form>
-      <TextField name="username" placeholder="Username"/>
-      <PasswordField name="password" placeholder="email" />
-      <DatePicker name="birthday" />
-      <Button onClick={onClickHandler}/>
-    </Form>);
+    const form = render(
+        <Form>
+            <TextField name="username" placeholder="Username"/>
+            <PasswordField name="password" placeholder="email" />
+            <DatePicker name="birthday" />
+            <Button onClick={onClickHandler}/>
+        </Form>
+    );
+
     expect(form).toMatchSnapshot();
   });
+
+    it('Form on submit', () => {
+
+        const onSubmitHandler = jestMock.fn();
+
+        const formComponent = mount(
+            <Form onSubmit={onSubmitHandler}>
+                <TextField name="username" placeholder="Username"/>
+                <PasswordField name="password" placeholder="email" />
+                <DatePicker name="birthday" />
+                <Button />
+            </Form>
+        );
+
+        const form = formComponent.find('form').first();
+        form.simulate('submit');
+
+        expect(onSubmitHandler).toBeCalled();
+    });
+
 });
